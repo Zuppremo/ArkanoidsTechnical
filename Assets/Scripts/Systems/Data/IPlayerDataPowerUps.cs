5bb3b6635dbddd470380117c8b675a8ba669cdb3
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-24 22:26:53 -0500

    Encapsulating data for UI and for the powerUps

---
+++ Assets/Scripts/Systems/Data/IPlayerDataPowerUps.cs
@@ -0,0 +1,7 @@
+public interface IPlayerDataPowerUps 
+{
+    void AddLives(int amount);
+    void RemoveLives(int amount);
+    void AddScore(int amount);
+    void RemoveScore(int amount);
+}
---
+++ Assets/Scripts/Systems/Data/IPlayerDataUI.cs
@@ -0,0 +1,5 @@
+public interface IPlayerDataUI 
+{
+    int Lives { get; }
+    int Score { get; }
+}
