57f9a9d6a3043436c0d26ef8a7af286db0fd6b02
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-25 15:54:09 -0500

    Powerup add live

--- Assets/Scripts/Systems/Gameplay/PowerUp/PowerUpPlayerLive.cs
+++ Assets/Scripts/Systems/Gameplay/PowerUp/PowerUpPlayerLive.cs
@@ -1,4 +1,3 @@
-using System.Net;
 using UnityEngine;
 
 public class PowerUpPlayerLive : PowerUpBase
@@ -6,15 +5,10 @@ public class PowerUpPlayerLive : PowerUpBase
     [SerializeField] private int livesToAdd = 1;
 
     private PlayerData playerData;
+
     private void Start()
     {
         playerData = FindObjectOfType<GameController>().PlayerData;
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        Activate();
-        gameObject.SetActive(false);
     }
 
     public override void Activate()
