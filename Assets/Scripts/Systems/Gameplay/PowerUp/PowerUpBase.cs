18d50419f29c1942c7d09eeccddd9fa36d097c02
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-25 00:29:55 -0500

    Improved base powerup

--- Assets/Scripts/Systems/Gameplay/PowerUp/PowerUpBase.cs
+++ Assets/Scripts/Systems/Gameplay/PowerUp/PowerUpBase.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public abstract class PowerUpBase : MonoBehaviour
 {
     public abstract void Activate();
+
+    public virtual void OnCollisionEnter(Collision collision)
+    {
+        Activate();
+        gameObject.SetActive(false);
+    }
 }
