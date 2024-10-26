2b196e3b5c0fd7dcb6819600508db5d525695f18
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-26 12:35:58 -0500

    Added events to know when its destroyed or just hit, also the chance to throw power ups

--- Assets/Scripts/Systems/Gameplay/Blocks/Block.cs
+++ Assets/Scripts/Systems/Gameplay/Blocks/Block.cs
@@ -1,8 +1,16 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour, IDamageable
 {
+    public event Action<Block> OnBlockDestroyed;
+    public event Action<Block> OnBlockHit;
+
     [SerializeField] private int maxHealth;
+    [SerializeField] private List<PowerUpBase> powerUps = new List<PowerUpBase>();
+    [Range(0F, 1F)][SerializeField] private float powerUpChance;
+
     private int currentHealth;
     public int CurrentHealth => currentHealth;
     public int MaxHealth => maxHealth;
@@ -15,8 +23,22 @@ public class Block : MonoBehaviour, IDamageable
     public void ReceiveDamage(int damage)
     {
         if (currentHealth > 0)
+        {
+            OnBlockHit?.Invoke(this);
             currentHealth -= damage;
+        }
         else
+        {
+            float randomPosibilityPowerUp = UnityEngine.Random.value;
+
+            if (randomPosibilityPowerUp < powerUpChance)
+            {
+                int randomGameObject = UnityEngine.Random.Range(0, powerUps.Count);
+                Instantiate(powerUps[randomGameObject], transform.position, Quaternion.identity);
+            }
+
+            OnBlockDestroyed?.Invoke(this);
             gameObject.SetActive(false);
+        }
     }
 }
