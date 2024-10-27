29787ab9e3cc5eb513033dc67ff7c3088831faa3
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-26 21:38:46 -0500

    added missing space to damager

--- Assets/Scripts/Systems/Gameplay/Blocks/Damager.cs
+++ Assets/Scripts/Systems/Gameplay/Blocks/Damager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Damager : MonoBehaviour
 {
     [SerializeField] private int damage = 1;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.TryGetComponent(out IDamageable damageable))
