bbb17927a3507b05a79ce13db8fa0a0d96b4ed74
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-27 00:13:07 -0500

    UI Controller update to diferent game states

--- Assets/Scripts/Systems/UI/UIController.cs
+++ Assets/Scripts/Systems/UI/UIController.cs
@@ -1,29 +1,49 @@
+using System;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] private GameObject losePanel = default;
     [SerializeField] private GameObject winPanel = default;
+    [SerializeField] private GameObject pausePanel = default;
 
     private GameController gameController;
 
     private void Awake()
     {
         gameController = FindObjectOfType<GameController>();
-        gameController.OnGameLost += HandleGameLost;
-        gameController.OnGameWon += HandleGameWon;
+        gameController.GameLost += OnGameLost;
+        gameController.GameWon += OnGameWon;
+        gameController.GamePaused += OnGamePaused;
+        gameController.GameUnpaused += OnGameUnpaused;
     }
 
-    private void HandleGameLost()
+    private void OnDestroy()
+    {
+        gameController.GamePaused -= OnGamePaused;
+        gameController.GameUnpaused -= OnGameUnpaused;
+        gameController.GameLost -= OnGameLost;
+        gameController.GameWon -= OnGameWon;
+    }
+
+    private void OnGamePaused()
+    {
+        pausePanel.SetActive(true);
+    }
+
+    private void OnGameUnpaused()
+    {
+        pausePanel.SetActive(false);
+
+    }
+
+    private void OnGameLost()
     {
         losePanel.SetActive(true);
-        gameController.OnGameLost -= HandleGameLost;
     }
 
-    private void HandleGameWon()
+    private void OnGameWon()
     {
         winPanel.SetActive(true);
-        gameController.OnGameWon -= HandleGameWon;
     }
-
 }
