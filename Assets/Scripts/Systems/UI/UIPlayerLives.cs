ec7b8c024e34f820358dd882b89c13496c256a1e
Deybi Montenegro <deibymontenegrob@gmail.com>
2024-10-28 15:59:09 -0500

    Hearts on lives implemented with UI

---
+++ Assets/Scripts/Systems/UI/UIPlayerLives.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerLives : MonoBehaviour
+{
+    [SerializeField] private GameObject playerHearth = default;
+    [SerializeField] private List<GameObject> playerLives = new List<GameObject>();
+    private PlayerData playerData;
+
+    private void Start()
+    {
+        playerData = FindObjectOfType<GameController>().PlayerData;
+        playerData.LifeAdded += OnLifeAdded;
+        playerData.LifeRemoved += OnLifeRemoved;
+    }
+
+    private void OnDestroy()
+    {
+        playerData.LifeAdded -= OnLifeAdded;
+        playerData.LifeRemoved -= OnLifeRemoved;
+    }
+
+    private void OnLifeRemoved()
+    {
+        playerLives[playerData.Lives].SetActive(false);
+    }
+
+    private void OnLifeAdded()
+    {
+        if (playerData.Lives >= playerData.MaxLives)
+            return;
+        playerLives[playerData.Lives].SetActive(true);
+    }
+}
+ 
